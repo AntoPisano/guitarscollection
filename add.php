3e47dfe863c1d327607ebf75e8db8a3f7fa13b19
Antonio <anto.pisano@hotmail.com>
2020-09-30 14:17:12 +0100

    story1update

--- add.php
+++ add.php
@@ -1,1 +1,10 @@
 <?php
+echo '<div><img class="images" src="' . $_POST['img'] . '" alt="' . '"/>Model: ' . $_POST['model'] . '<br>Year: ' . $_POST['year'] . '<br>Type: ' . $_POST['type'] . '<br>' .
+    'Color: ' . $_POST['color'] . '<br>Played by: ' . $_POST['playedby'] . '</div>';
+
+
+
+
+
+
+?>
--- collection.php
+++ collection.php
@@ -3,24 +3,21 @@
 <head>
     <title>Collection</title>
     <link rel="stylesheet" type="text/css" href="collection.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
 </head>
 <body>
 <header>
-    <nav>
         <img class="guitar" src = "https://w0286994blog.files.wordpress.com/2016/04/scute.jpg?w=676" alt="Guitarlogo"></li>
         <h1 class="title">THE GUITAR COLLECTION!!!!!</h1>
-    </nav>
 </header>
-<form action="http://localhost:1234/collection/guitarscollection/form.php">
-<button type="submit">the pig wants more rock<br>ADD GUITAR!</button>
-</form>
+<a href="http://localhost:1234/collection/guitarscollection/form.php" class="button">the pig wants more rock<br>ADD GUITAR!</a>
 <div class="collection">
     <?php
     if (!empty($result)) {
         foreach ($result as $guitar) {
             echo '<div><img class="images" src="' . $guitar['img'] . '" alt="' . $guitar['model'] . '"/>
-        Model: ' . $guitar['model'] . '<br>Year: ' . $guitar['year'] . '<br>Type: ' . $guitar['type'] . '<br>' .
-                'Color: ' . $guitar['color'] . '<br>Played by: ' . $guitar['artist'] . '</div>';
+        Model: ' . $guitar['model'] . '<div>Year: ' . $guitar['year'] .'</div>' . 'Type: ' . $guitar['type'] . '<div>' .
+                'Color: ' . $guitar['color'] .'</div>' . '<div>Played by: ' . $guitar['artist'] .'</div>' . '</div>';
         }
     } else {
         echo '<h1>Database connection error try again later</h1>';
