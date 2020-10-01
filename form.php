2ee12d5df166f0a16293e527ced33697850f2725
Antonio <anto.pisano@hotmail.com>
2020-10-01 11:20:22 +0100

    story2done

--- form.php
+++ form.php
@@ -3,36 +3,22 @@
     <link rel="stylesheet" type="text/css" href="collection.css">
 </head>
 <body>
-
 <h1 class="addGuitar">Add a guitar to the collection:</h1>
 <div class="container">
 <form action="add.php" method="POST">
     <ul>
-        <li><input type="text" name="img" placeholder="IMG LINK" formmethod="POST"></li>
-        <li><input type="text" name="model" placeholder="MODEL" formmethod="POST"></li>
-        <li><input type="text" name="year" placeholder="YEAR" formmethod="POST"></li>
-        <li><input type="text" name="type" placeholder="TYPE" formmethod="POST"></li>
-        <li><input type="text" name="color" placeholder="COLOR" formmethod="POST"></li>
-        <li><input type="text" name="playedby" placeholder="PLAYED BY" formmethod="POST"></li>
+        <li><input type="text" name="img" placeholder="IMG LINK"></li>
+        <li><input type="text" name="model" placeholder="MODEL"></li>
+        <li><input type="text" name="year" placeholder="YEAR"></li>
+        <li><input type="text" name="type" placeholder="TYPE"></li>
+        <li><input type="text" name="color" placeholder="COLOR"></li>
+        <li><input type="text" name="artist" placeholder="PLAYED BY"></li>
     </ul>
-    <button type="submit">ADD GUITAR</button>
+    <button type="submit">ADD GUITAR!!</button>
 </form>
 </div>
     <img class="guitar" src = "https://thumbs.dreamstime.com/b/rock-roll-lettering-cartoon-pig-rock-roll-lettering-cartoon-pig-eps-rgb-global-colors-113875550.jpg" alt="Guitarlogo"></li>
-    <form action="http://localhost:1234/collection/guitarscollection/collection.php">
-        <button type="submit">BACK TO GUITAR COLLECTION</button>
-    </form>
-
-
-
-</div>
-
-
-
-
-
-
-
+    <a href="http://localhost:1234/collection/guitarscollection/collection.php" class="button">BACK TO GUITAR COLLECTION!!</a>
 
 </body>
 </html>
