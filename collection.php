4a0e934d46c30bb417eabc1e628b1d4d5676d2d2
Antonio <anto.pisano@hotmail.com>
2020-11-28 01:24:25 +0000

    new background-color

--- collection.php
+++ collection.php
@@ -8,7 +8,7 @@
 <body>
 <header>
         <img class="guitar" src = "https://www.bbop.eu/files/posts/77/images/vince_gill_bbop-9-large.jpg?1511798725" alt="Guitarlogo"></li>
-        <h1 class="title">THE GUITAR COLLECTION!!!!!</h1>
+        <h1 class="title">THE GUITAR COLLECTION!!</h1>
 </header>
 <a href="http://localhost:1234/collection/guitarscollection/form.php" class="button">ADD GUITAR!</a>
 <div class="collection">
--- form.php
+++ form.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" type="text/css" href="collection.css">
 </head>
 <body>
-<h1 class="addGuitar">Add a guitar to the collection:</h1>
+<h1 class="addGuitar">Add a new guitar to the collection:</h1>
 <div class="container">
 <form action="add.php" method="POST">
     <ul>
@@ -18,7 +18,7 @@
 </form>
 </div>
     <img class="guitar" src = "https://thumbs.dreamstime.com/b/rock-roll-lettering-cartoon-pig-rock-roll-lettering-cartoon-pig-eps-rgb-global-colors-113875550.jpg" alt="Guitarlogo"></li>
-    <a href="http://localhost:1234/collection/guitarscollection/collection.php" class="button">BACK TO GUITAR COLLECTION!!</a>
+    <a href="http://localhost:1234/collection/guitarscollection/collection.php" class="button">BACK TO THE GUITAR COLLECTION!!</a>
 
 </body>
 </html>
